7ca9c53ade96144e5aff48b21f0fdadedb3fa2ad
Antonio Silva <124893465+tombemol@users.noreply.github.com>
2026-09-20 10:18:13 -0300

    fix: expose Vite dev server for Tauri Android

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const host = process.env.TAURI_DEV_HOST;
+
 export default defineConfig({
   plugins: [react()],
   clearScreen: false,
   server: {
+    host: host || false,
     strictPort: true,
     port: 1420,
+    hmr: host
+      ? {
+          protocol: 'ws',
+          host,
+          port: 1421,
+        }
+      : undefined,
+    watch: {
+      ignored: ['**/src-tauri/**'],
+    },
   },
 });
